b14c04cc9612d8daa8dfca000edee6fd25e5d7af
Christopher Leynes <leynestopher12@gmail.com>
2026-09-16 01:55:15 +0800

    Add files via upload

--- photo.js
+++ photo.js
@@ -3,7 +3,7 @@
 
 const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxdI7YGhVzL4_ljSNDTAGLGvpK7Q3nLPBZoCAwXFMTWV1EewQB2iycxKA6QfQdDXaHM/exec';
 const WEDDING_EMAIL = 'queencypineda29@gmail.com';
-const HASHTAG = '#CenFoundHisQueenCy';
+const HASHTAG = '#CenFoundHisQueency';
 const FRAME_W = 1600;
 const FRAME_H = 1068;
 const SHOT_COUNT = 3;
